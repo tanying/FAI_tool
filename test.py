792d075dd0a2debc5a0c1e0c0cc8fe34fa633469
ying.tan <ying.tan@tcl.com>
2014-09-26 17:04:39 +0800

    add test.py

--- test.py
+++ test.py
@@ -2,3 +2,13 @@
 # -*- coding: utf8 -*-
 
 from xml.etree import ElementTree
+
+file = 'bin/collection_golden.txt'
+
+f = open(file, 'r')
+text = f.read()
+f.close()
+
+root = ElementTree.fromstring(text)
+varNodes = root.getiterator('VAR')
+for node in varNodes:
