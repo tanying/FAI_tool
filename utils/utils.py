fd518c7931cf463a334e576701bba56275ee90c4
tanying <tanya.xihe@gmail.com>
2014-09-27 21:13:45 +0800

    add change

--- utils/utils.py
+++ utils/utils.py
@@ -29,58 +29,6 @@ def is_pull_aready(path):
     else:
         return False
 
-def pull_by_apk(cfile):
-
-    if is_pull_aready('golden_collection.flag'):
-        os.system('adb pull /sdcard/golden_collection.txt ./%s' % cfile)
-        os.system('adb uninstall com.tcl.goldencollector')
-        os.system('adb shell rm -f /data/local/tmp/goldencollector.apk')
-        os.system('adb shell rm -f /sdcard/golden_collection.flag')
-        os.system('adb shell rm -f /sdcard/golden_collection.txt')
-    else:
-        pull_by_apk(cfile)
-
-def pull_info_from_phone(is_golden):
-    global PULL_SUCCESS_FLAG
-
-    if is_golden:
-        property_file = GOLDEN_PROPERTY
-        collection_file = GOLDEN_COLLECTION
-        unconnected_hint = 'Please connect the Golden Phone with PC by USB.'
-        success_hint = 'Pull properties from golden phone successful.\n'
-    else:
-        property_file = SAMPLE_PROPERTY
-        collection_file = SAMPLE_COLLECTION
-        unconnected_hint = 'Please connect the Sample Phone with PC by USB for comparation.'
-        success_hint = 'Pull properties from sample phone successful.\n'
-
-    command = 'adb shell getprop > %s' % property_file
-    status, output = commands.getstatusoutput(command)
-
-    if status == 0:
-        os.system('adb install utils/goldencollector.apk')
-        os.system('adb shell am start -n com.tcl.goldencollector/.MainActivity')
-
-        pull_by_apk(collection_file)
-
-        PULL_SUCCESS_FLAG = True
-        print success_hint
-    else:
-        print unconnected_hint
-        #restart server
-        os.system('sudo adb kill-server')
-        os.system('sudo adb start-server')
-
-        usb_connected = raw_input('enter(aleady), else(exit)')
-
-        if usb_connected == '':
-            pull_info_from_phone(is_golden)
-        else:
-            print 'You pressed any key to exit, bye!'
-            sys.exit(1)
-
-
-
 def change_file_to_dict(filepath):
     fdict = {}
     f = open(filepath, 'r')
@@ -167,9 +115,9 @@ def generate_out_put_string(diff, string):
     if list0:
         string += '  Golden Phone and Sample Phone have different value.\n'
         for info in list0:
-            string += '    key: %s\n' % info['key']
-            string += '      golden_value: %s\n' % info['golden_value']
-            string += '      sample_value: %s\n' % info['sample_value']
+            string += '    key: [%s]\n' % info['key']
+            string += '      golden_value: [%s]\n' % info['golden_value']
+            string += '      sample_value: [%s]\n' % info['sample_value']
 
     if list1:
         string += '\n  Info exists in Golden but not in Sample.\n'
@@ -196,17 +144,16 @@ def output_diff_to_file(diff, file):
 
     else:
         is_pass = 'FAIL'
-        result_string += 'FAIL\n\n'
-
+        result_string += 'FAIL'
 
         if not r0:
-            result_string += '[system property difference]\n'
+            result_string += '\n\n[system property difference]\n'
             result_string = generate_out_put_string(diff['sysprop'], result_string)
         if not r1:
-            result_string += '[settings difference]\n'
+            result_string += '\n\n[settings difference]\n'
             result_string = generate_out_put_string(diff['settings'], result_string)
         if not r2:
-            result_string += '[ringtone difference]\n'
+            result_string += '\n\n[ringtone difference]\n'
             result_string = generate_out_put_string(diff['ringtone'], result_string)
 
     f = open(file, 'wb')
@@ -216,37 +163,3 @@ def output_diff_to_file(diff, file):
 
     print 'FAI RESULT: %s' % is_pass
     print 'The detail report have output successfully in [%s]' % file
-
-def parse_collection_txt(file):
-    fdict = {}
-
-    sdict = {}
-    rdict = {}
-
-    f = open(file, 'r')
-    flist = f.readlines()
-    f.close()
-
-    key = ''
-    count = 0
-    for item in flist:
-        mod = count % 2
-        if mod == 0:
-            print 
-            if item[:len(SETTINGS_KEY_TAG)] == SETTINGS_KEY_TAG:
-                key = filter_xml_tag(SETTINGS_KEY_TAG, item)
-            elif item[:len(RINGTONE_KEY_TAG)] == RINGTONE_KEY_TAG:
-                key = filter_xml_tag(RINGTONE_KEY_TAG, item)
-        else:
-            if item[:len(SETTINGS_VAL_TAG)] == SETTINGS_VAL_TAG:
-                val = filter_xml_tag(SETTINGS_VAL_TAG, item)
-                sdict[key] = val
-            elif item[:len(RINGTONE_VAL_TAG)] == RINGTONE_VAL_TAG:
-                val = filter_xml_tag(RINGTONE_VAL_TAG, item)
-                rdict[key] = val
-
-        count += 1
-
-    fdict['settings'] = sdict
-    fdict['ringtone'] = rdict
-    return fdict
